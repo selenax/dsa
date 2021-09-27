84095e86bdecf5910c864923d7c77507a5edaad2
selenax <selenawenxu@gmail.com>
2021-09-27 11:43:06 -0700

    Completed

---
+++ math/addBigNumber.js
@@ -0,0 +1,14 @@
+function addStrings(string1, string2) {
+  let result = 0;
+  if(string1.indexOf('e') !== -1) {
+      string1 = parseFloat(string1);
+      result = string1 * Math.pow(10, parseFloat(string2))
+  } else {
+     result = parseFloat(string1) + parseFloat(string2) 
+  }
+return result + '';
+}
+
+
+console.log(addStrings('2e', '32'))
+console.log(addStrings('3.2', '3.33'))
