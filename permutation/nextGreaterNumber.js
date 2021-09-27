1cb0ff4a83c11de2bbf88662f00f8e8afb4a731d
selenax <selenawenxu@gmail.com>
2021-09-26 23:02:13 -0700

    Completed

--- permutation/nextGreaterNumber.js
+++ permutation/nextGreaterNumber.js
@@ -2,9 +2,10 @@
 // For example, suppose the input is 423862, then the biggest number with these digits is 426238.
 
 
-// ie if not working with same set of digits: 34681 => next greater is 34682 
+// !ie if not working with same set of digits: 34681 => next greater is 34682 
   // so it make sense to start from right to left 
 
+// O(N)
 function nextGreaterNumber(num) {
 //check from right to left for descending order
   //find index that's out of order
@@ -16,10 +17,10 @@ function nextGreaterNumber(num) {
   let str = num.toString().split('');
   
   for(let x = str.length-1; x > 0; x--) {
-    console.log(str[x-1] < str[x], str[x-1], str[x]);
     if(str[x-1] < str[x]) {
       currIndex = x-1;
-      currVal = str[x-1]
+      currVal = str[x-1];
+      largestSubIndex = x;
       break;
     }
 
@@ -27,13 +28,11 @@ function nextGreaterNumber(num) {
 //all digit are descending, nothing to swap for next greater value 
 if(!currIndex) return -1 
 
-let nextIndex = str.length-1;
-
-console.log(nextIndex);
+let nextIndex = largestSubIndex;
 
 for(let y = str.length-1; y > currIndex; y--) {
-  if(str[y] > currVal && str[y] < str[nextIndex]) {
-    nextIndex = y;    
+if(str[y] > currVal && str[y] < str[nextIndex]) {
+  nextIndex = y;   
   }
 }
 
@@ -41,10 +40,9 @@ let temp = str[currIndex];
 str[currIndex] = str[nextIndex];
 str[nextIndex] = temp;
 
-return str.join('')
- 
+return Number(str.join(''))
 }
 
 console.log(nextGreaterNumber(12531)); //13521
-// console.log(nextGreaterNumber(54321)); //-1
-// console.log(nextGreaterNumber(534976));
+console.log(nextGreaterNumber(54321)); //-1
+console.log(nextGreaterNumber(534976)); //536974
