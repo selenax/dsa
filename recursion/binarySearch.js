0c922af771ce2404e2e90fd5df127d402b2bf7f3
selenax <selenawenxu@gmail.com>
2021-09-28 23:04:18 -0700

    Completed

--- recursion/binarySearch.js
+++ recursion/binarySearch.js
@@ -17,19 +17,18 @@ function binarySearch(arr, k) {
     //else no match found, return -1  
     let start = 0;
     let end = arr.length;
-    while(start <= end) {
+    while(start < end) {
       let mid = Math.floor((start + end)/2)
-      console.log(mid);
       if(arr[mid] === k) {
-        return mid;
+        return `found in index ${mid}`;
       } else if (arr[mid] < k) {
         start = mid;
       } else if(arr[mid] > k) {
         end = mid;
       } else {
-        return -1
+        return `not found`
       }
     }
 }
 
-console.log(binarySearch([0,1,2,3,4,6,7,8,9], 9));
+console.log(binarySearch([0,1,2,3,4,5,6,7,8,9,10,11], 6));
