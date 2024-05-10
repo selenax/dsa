a44a4a1d50fc2743eee4f5ce23441315f3506129
Selena X <selenaxx20@gmail.com>
2024-05-09 21:37:00 -0500

    recursion

---
+++ binarySearch/rotatedBinararySearch.js
@@ -0,0 +1,14 @@
+// Rotated Array Binary Search
+// Search an element in a sorted and rotated array.
+
+// Example 1:
+// Input: nums = [4,5,6,7,0,1,2], target = 0
+// Output: 4
+// Example 2:
+// Input: nums = [4,5,6,7,0,1,2], target = 3
+// Output: -1
+// Example 3:
+// Input: nums = [1], target = 0
+// Output: -1
+
+// [10,20,1,3,6,7,8]
