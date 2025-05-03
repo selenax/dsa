27aa6a77ee3ed1c2b3fc2a1239d0733f81f9347e
Selena X <selenaxx20@gmail.com>
2025-05-02 19:50:57 -0500

    completed

--- Problem_Solving_Patterns/Sliding_Window/maxSubarraySum.js
+++ Problem_Solving_Patterns/Sliding_Window/maxSubarraySum.js
@@ -21,35 +21,37 @@ function maxSubarraySum(arr, n) {
 }
   return max; 
 }
-
-//! time:O(N) space:O(1)
-function maxSubarraySum(arr, n) {
-  // 1st loop accumulate first n number sum
-  let sum = 0;
-  for(let i = 0; i < n; i++) {
-    sum += arr[i]
-  }
-
-  // 2nd loop start at n
-  // add next index, subtract first index from sum
-  // n = 3 [0 , 1, 2]
-    //  [0,1,2 3, 4] // current = 4, subtract 4-N = 1
-  // index 3 -> add to sum, subtract index 0 => current index(3) - N 
-  // compare max 
-  let max = sum
-  for(let j = n; j < arr.length; j++) {
-    sum += arr[j] - arr[j-n] 
-    // sum += arr[j]
-    // sum -= arr[j-n]
-    max = Math.max(sum, max)
-  }
-  return max;
-}
-
-
 console.log(maxSubarraySum([1,2,5,2,8,1,5], 2)); //10
-console.log(maxSubarraySum([1,2,5,2,8,1,5], 4)); //17
-console.log(maxSubarraySum([], 4)); //null
-console.log(maxSubarraySum([4,2,1,6], 1)); //6
-console.log(maxSubarraySum([-4,2,1,-6], 3)); //-1
+
+// //! time:O(N) space:O(1)
+// //whenever we add a new value, we subtract the first value to get the new sum
+// function maxSubarraySum(arr, n) {
+//   // 1st loop accumulate first n number sum
+//   let sum = 0;
+//   for(let i = 0; i < n; i++) {
+//     sum += arr[i]
+//   }
+
+//   // 2nd loop start at n
+//   // add next index, subtract first index from sum
+//   // n = 3 [0 , 1, 2]
+//     //  [0,1,2 3, 4] // current = 4, subtract 4-N = 1
+//   // index 3 -> add to sum, subtract index 0 => current index(3) - N 
+//   // compare max 
+//   let max = sum
+//   for(let j = n; j < arr.length; j++) {
+//     sum += arr[j] - arr[j-n] 
+//     // sum += arr[j]
+//     // sum -= arr[j-n]
+//     max = Math.max(sum, max)
+//   }
+//   return max;
+// }
+
+
+// console.log(maxSubarraySum([1,2,5,2,8,1,5], 2)); //10
+// console.log(maxSubarraySum([1,2,5,2,8,1,5], 4)); //17
+// console.log(maxSubarraySum([], 4)); //null
+// console.log(maxSubarraySum([4,2,1,6], 1)); //6
+// console.log(maxSubarraySum([-4,2,1,-6], 3)); //-1
                            
