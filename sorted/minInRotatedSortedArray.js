f3072cd05a7935c322b280669cc70171e1904ea9
Selena X <selenaxx20@gmail.com>
2025-05-02 19:52:43 -0500

    completed

--- sorted/minInRotatedSortedArray.js
+++ sorted/minInRotatedSortedArray.js
@@ -1,31 +1,61 @@
 // 153. Find Minimum in Rotated Sorted Array - Medium
 
+// const findMin = (nums) => {
+//     //initialize pointers 
+//     let left = 0, right = nums.length-1;
+
+//     while(left < right) {
+//         //base case: 
+//         //if nums[0] < nums[len-1] meaning it's already sorted
+//         if(nums[left] < nums[right]) return nums[left]
+
+//         //find mid value; 
+//         let mid = Math.floor((left + right)/2)
+
+//          // mid > r, pivot is on the right, update left = mid+1
+//          if(nums[mid] > nums[right]) {
+//             left = mid + 1
+//         // else mid <= n[0], pivot = left half, update right = mid;
+//          } else {
+//             right = mid;
+//          }
+//     }
+//     return nums[left]
+// }
 
 
-//   0           (n-1)
-// [left < mid < right]
-//  smallest          biggest 
 
-const findMin = (nums) => {
-    //initialize pointers 
-    let left = 0, right = nums.length-1;
+/*
+lo hi
+[3,2]
+   lr
 
-    while(left < right) {
-        //if nums[0] < nums[len-1] meaning it's already sorted
-        if(nums[left] < nums[right]) return nums[left]
+ [3,1,2] 
+  s m e
+    s
+*/   
+const findMin = arr => {
+//use 2 point initially pointing them at first and last element, start and end, respectfully
 
-        //find mid value; 
-        let mid = Math.floor((left + right)/2)
+//modified binary search: 
+//while start < end 
+    // base case: arr[s] < arr[e] return arr[s]
+    // find mid value 
+    // mid > end, start = mid + 1 
+    // else, s = mid;
+let start = 0, end = arr.length-1;
 
-         // mid > r, pivot is on the right, update left = mid+1
-         if(nums[mid] > nums[right]) {
-            left = mid + 1
-        // else mid <= n[0], pivot = left half, update right = mid;
-         } else {
-            right = mid;
-         }
-    }
-    return nums[left]
+while(start < end) {
+    if(arr[start] < arr[end]) return arr[start];
+
+    let mid = Math.floor((start + end)/2);
+
+    if(arr[mid] > arr[end]) start = mid + 1; 
+    else start = mid;
+
+}
+return arr[start]
+
 }
 
 
