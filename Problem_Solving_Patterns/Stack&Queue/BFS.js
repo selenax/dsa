8f3711c8201940a9e8a70eadfc253af4bdc38552
Selena X <selenaxx20@gmail.com>
2025-05-05 20:17:32 -0500

    completed

--- Problem_Solving_Patterns/Stack&Queue/BFS.js
+++ Problem_Solving_Patterns/Stack&Queue/BFS.js
@@ -36,16 +36,5 @@ function bfs(root) {
 //     return { value, left, right };
 //   }
   
-
-  const tree = createNode(1,
-    createNode(2,
-      createNode(4),
-      createNode(5)
-    ),
-    createNode(3,
-      createNode(6),
-      createNode(7)
-    )
-  );
   console.log(bfs(tree));
   
