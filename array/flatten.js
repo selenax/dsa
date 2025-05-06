7350788913e984b3b9d37fad25e337e16ced5c0c
Selena X <selenaxx20@gmail.com>
2025-05-06 17:27:42 -0500

    completed

--- array/flatten.js
+++ array/flatten.js
@@ -16,14 +16,16 @@ const flattenArray = (arr) => {
 	return result;
 };
 
-console.log(flattenArray([1, [2], [3, [[4]]]])); //[ 1, 2, 3, 4 ]
-console.log(flattenArray([[['a']], [['b']]])); //[ 'a', 'b' ]
+// console.log(flattenArray([1, [2], [3, [[4]]]])); //[ 1, 2, 3, 4 ]
+// console.log(flattenArray([[['a']], [['b']]])); //[ 'a', 'b' ]
 
 //refactor
 const flattenArray2 = (arr) => {
 	let flatten = [].concat(...arr);
+    // console.log(flatten);
+    
 	return flatten.some(Array.isArray) ? flattenArray2(flatten) : flatten;
 };
 
 console.log(flattenArray2([1, [], [3, [[4]]]])); // [1, 3, 4].
-console.log(flattenArray2([1, {}, [3, [[4]]]])); // [1, {}, 3, 4]
+// console.log(flattenArray2([1, {}, [3, [[4]]]])); // [1, {}, 3, 4]
