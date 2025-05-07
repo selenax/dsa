e75ac968f60ba9965d33d8203b97a84b8114cce1
Selena X <selenaxx20@gmail.com>
2025-05-07 18:20:50 -0500

    completed

--- array/chunks.js
+++ array/chunks.js
@@ -1,6 +1,6 @@
 // Write a function that splits an array (first argument) into groups the length of size (second argument) and returns them as a two-dimensional array.
 
-//ALGORITHM TIME:O(N) SPACE:O(N)
+// ALGORITHM TIME:O(N) SPACE:O(N)
 const chunks = (arr, size) => {
 	let subArray = [];
 	let result = [];
@@ -20,10 +20,21 @@ const chunks = (arr, size) => {
 	return result;
 };
 
+// refactor
+const chunks2 = (arr, size) => {
+	let result = [];
+	//increment x by size
+	for (let x = 0; x < arr.length; x += size) {
+		//slice by size and push subarray to result
+		result.push(arr.slice(x, x + size));
+	}
+	return result;
+};
+
 console.log(chunks(['a', 'b', 'c', 'd'], 2)); //[["a", "b"], ["c", "d"]].
 console.log(chunks([0, 1, 2, 3, 4, 5], 3)); //[[0, 1, 2], [3, 4, 5]].
 console.log(chunks([0, 1, 2, 3, 4, 5], 2)); //[[0, 1], [2, 3], [4, 5]].
 console.log(chunks([0, 1, 2, 3, 4, 5], 4)); //[[0, 1, 2, 3], [4, 5]].
 console.log(chunks([0, 1, 2, 3, 4, 5, 6], 3)); //[[0, 1, 2], [3, 4, 5], [6]].
-console.log(chunks([0, 1, 2, 3, 4, 5, 6, 7, 8], 4)); //[[0, 1, 2, 3], [4, 5, 6, 7], [8]].
-console.log(chunks([0, 1, 2, 3, 4, 5, 6, 7, 8], 2)); //[[0, 1], [2, 3], [4, 5], [6, 7], [8]].
+console.log(chunks2([0, 1, 2, 3, 4, 5, 6, 7, 8], 4)); //[[0, 1, 2, 3], [4, 5, 6, 7], [8]].
+console.log(chunks2([0, 1, 2, 3, 4, 5, 6, 7, 8], 2)); //[[0, 1], [2, 3], [4, 5], [6, 7], [8]].
