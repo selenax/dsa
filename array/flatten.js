723e0ad1e8d909e91ffff72e21c187e791916e89
Selena X <selenaxx20@gmail.com>
2025-05-08 20:46:03 -0500

    refactor

--- array/flatten.js
+++ array/flatten.js
@@ -1,31 +1,78 @@
-//Given nested arrays, return all values in one single array;
+// //Given nested arrays, return all values in one single array;
 
+// console.log(flattenArray([1, [2], [3, [[4]]]])); //[ 1, 2, 3, 4 ]
+// console.log(flattenArray([[['a']], [['b']]])); //[ 'a', 'b' ]
+
+// //refactor
+// const flattenArray2 = (arr) => {
+// 	let flatten = [].concat(...arr);
+// 	// console.log(flatten);
+
+// 	return flatten.some(Array.isArray) ? flattenArray2(flatten) : flatten;
+// };
+
+// TIME:O(N+D) SPACE:O(N+D); D=depth of nesting
 const flattenArray = (arr) => {
+	//inital empty array to store result;
 	let result = [];
+	//since arrays deeply nested, recursion can be used to dive into each one
+	//create helper function to recurse until given paramenter is not an array
+	//if arg is not an array, push arg to result
+	//else, we have an array
+	//loop over array, recurse on each item
 
 	function checkArray(arg) {
 		if (!Array.isArray(arg)) {
 			result.push(arg);
 		} else {
-			for (let index in arg) {
-				checkArray(arg[index]);
+			for (let item of arg) {
+				checkArray(item);
 			}
 		}
 	}
+
+	//loop over array and pass helper function to each item
 	arr.forEach(checkArray);
+
+	//return result;
 	return result;
 };
 
-// console.log(flattenArray([1, [2], [3, [[4]]]])); //[ 1, 2, 3, 4 ]
-// console.log(flattenArray([[['a']], [['b']]])); //[ 'a', 'b' ]
-
-//refactor
+// refactor
+// TIME: O(N); worst case:O(N + D), D=depth of nesting
 const flattenArray2 = (arr) => {
-	let flatten = [].concat(...arr);
-    // console.log(flatten);
-    
-	return flatten.some(Array.isArray) ? flattenArray2(flatten) : flatten;
+	//concat flattens 1 level deep, so each call flattens 1 level
+	let result = [].concat(...arr);
+
+	//some() checks to see if another level of flatten is needed;
+	//if some returns true, recurse over result
+	//else no more array, return results
+	return result.some(Array.isArray) ? flattenArray2(result) : result;
 };
 
-console.log(flattenArray2([1, [], [3, [[4]]]])); // [1, 3, 4].
+// console.log(flattenArray2([1, [2], [3, [[4]]]])); //[ 1, 2, 3, 4 ]
+// console.log(flattenArray([[['a']], [['b']]])); //[ 'a', 'b' ]
+// console.log(flattenArray([1, [], [3, [[4]]]])); // [1, 3, 4]
 // console.log(flattenArray2([1, {}, [3, [[4]]]])); // [1, {}, 3, 4]
+
+// refactor: USING STACK
+// TIME:O(N) SPACE:O(N)
+const flattenArray3 = (arr) => {
+	let result = [];
+	// create copy to avoid mutation
+	let stack = [...arr];
+
+	while (stack.length > 0) {
+		const current = stack.pop();
+		if (Array.isArray(current)) {
+			// add all items inside array using spread
+			stack.push(...current);
+		} else {
+			result.push(current);
+		}
+	}
+
+	// elements were added in reverse order
+	return result.reverse();
+};
+console.log(flattenArray3([3, [[4]]]));
