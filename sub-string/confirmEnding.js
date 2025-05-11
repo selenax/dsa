3881117c1f5746beda8aaacc8c9d62ce06b77cdb
Selena X <selenaxx20@gmail.com>
2025-05-10 20:47:49 -0500

    refactored

--- sub-string/confirmEnding.js
+++ sub-string/confirmEnding.js
@@ -7,8 +7,9 @@
 //"Open sesame", "pen" -> false
 //"Connor", "n" -> false
 
-// TIME:O(k), k=substring of target length;
-// SPACE:O(1), temp string O(k) = constant overhead that doesn't scale w/ input size
+// ALGORITHM: USING SUBSTRING
+// TIME:O(K), k=substring of target length;
+// SPACE:O(1), temp string = O(K) = constant overhead since doesn't scale w/ input size
 const confirmEnding = (str, target) => {
 	//get the length of target
 	const targetLen = target.length;
@@ -19,8 +20,34 @@ const confirmEnding = (str, target) => {
 	return str.substring(str.length - targetLen) === target;
 };
 
+// ALGORITHM: USING LOOP
+// TIME:O(N) SPACE:(1)
+const confirmEnding2 = (str, target) => {
+	//if target len is greater than str len, return false
+	if (target.length > str.length) return false;
 
+	// option 1:
+	// traverse backwards using target to match with str
+	// let strLen = str.length;
+	// for (let i = target.length - 1; i >= 0; i--) {
+	// 	if (target[i] !== str[--str.length]) {
+	// 		return false;
+	// 	}
+	// }
 
-console.log(confirmEnding('Bastian', 'n'));
-console.log(confirmEnding('Open sesame', 'same'));
-console.log(confirmEnding('Open sesame', 'pen'));
+	// option 2:
+	// traverse from ending string
+	for (let i = 0; i < target.length; i++) {
+		if (target[i] !== str[str.length - target.length + i]) return false;
+	}
+	return true;
+};
+
+// ALGORITHM: USING endsWith
+// const confirmEnding3 = (str, target) => {
+//     return str.endsWith(target)
+// }
+
+console.log(confirmEnding2('Bastian', 'n'));
+console.log(confirmEnding2('Open sesame', 'same'));
+console.log(confirmEnding2('Open sesame', 'pen'));
