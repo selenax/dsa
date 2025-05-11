62a394251e9e3fec5ad54f7ce9fcb82b999901dc
Selena X <selenaxx20@gmail.com>
2025-05-11 18:21:09 -0500

    completed

--- subString/versionControl.js
+++ subString/versionControl.js
@@ -9,6 +9,7 @@
 // ('12.0.1', '12.0.1.1.0') -> '12.0.1.1.0'
 // ('1.2.2', '1.3') -> 1.3
 
+//TIME:O(N) SPACE:O(N+M)
 const versionControl = (v1, v2) => {
 	//split each parameter into array by '.' and convert each digit into numbers
 	const v1Arr = v1.split('.').map(Number);
@@ -27,11 +28,36 @@ const versionControl = (v1, v2) => {
 	return 0; //equal
 };
 
-console.log(versionControl('20', '2.0.4')); //'20'
-console.log(versionControl('1', '0.0.4')); //'1'
-console.log(versionControl('1.3', '1.2.2')); //'1.3'
-console.log(versionControl('2.0.1', '2.0.1')); //'2.0.1' (equal)
-console.log(versionControl('1.1.2', '1.1')); //'1.1.2'
-console.log(versionControl('1.2.2', '1.3')); //'1.3'
-console.log(versionControl('12.0.1.0', '12.0.1')); //'12.0.1' (equal)
-console.log(versionControl('12.0.1', '12.0.1.1.0')); //'12.0.1.1.0'
+// return:
+// v1 if v1 > v2
+// v2 if v1 < v2
+// shorter version if equal
+
+const versionControl2 = (v1, v2) => {
+	// split string
+	const v1Arr = v1.split('.').map(Number);
+	const v2Arr = v2.split('.').map(Number);
+
+	// traverse the longer string when comparing string of diff lengths
+	const maxLen = Math.max(v1Arr.length, v2Arr.length);
+
+	for (let i = 0; i < maxLen; i++) {
+		// account for when out of bound
+		const num1 = v1Arr[i] || 0;
+		const num2 = v2Arr[i] || 0;
+
+		if (num1 > num2) return v1;
+		if (num1 < num2) return v2;
+	}
+	// return shorter version if equal
+	return v1Arr.length <= v2Arr.length ? v1 : v2;
+};
+
+console.log(versionControl2('20', '2.0.4')); //'20'
+console.log(versionControl2('1', '0.0.4')); //'1'
+console.log(versionControl2('1.3', '1.2.2')); //'1.3'
+console.log(versionControl2('2.0.1', '2.0.1')); //'2.0.1' (equal)
+console.log(versionControl2('1.1.2', '1.1')); //'1.1.2'
+console.log(versionControl2('1.2.2', '1.3')); //'1.3'
+console.log(versionControl2('12.0.1.0', '12.0.1')); //'12.0.1' (equal)
+console.log(versionControl2('12.0.1', '12.0.1.1.0')); //'12.0.1.1.0'
