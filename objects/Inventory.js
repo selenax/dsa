11b34141930609695d02f3aa35f34bdd554b4e78
Selena X <selenaxx20@gmail.com>
2025-05-25 20:19:28 -0700

    added test cases

--- objects/Inventory.js
+++ objects/Inventory.js
@@ -44,3 +44,14 @@ Inventory.prototype.checkItem = function (item) {
 	if (this[item]) return this[item];
 	else return 'Item is not in inventory';
 };
+
+
+
+const myInventory = new Inventory('cucumber', 2);
+myInventory.addItem('carrot', 1);
+console.log(myInventory.checkItem('cucumber')); // Logs: { price: 2, quantity: 1 }
+myInventory.addItem('cucumber', 3); 
+console.log(myInventory.deleteItem('carrot')); // Logs: 'Deleted'
+console.log(myInventory.deleteItem('carrot')); // Logs: 'Nothing to delete'
+console.log(myInventory); // Logs: { cucumber: { price: 3, quantity: 2 }, carrot: { price: 1, quantity: 0 } }
+console.log(myInventory.checkItem('radish')); // Logs: 'Item is not in inventory'
