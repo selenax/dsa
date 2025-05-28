cd49f7730bc251f7f87587eefb54c8d94bcf2523
Selena X <selenaxx20@gmail.com>
2025-05-27 18:18:25 -0700

    completed O(N) time

--- subString/longestSubstringWithoutRepatingChars.js
+++ subString/longestSubstringWithoutRepatingChars.js
@@ -15,28 +15,28 @@
 // Input: s = "pwwkew"
 // Output: 3
 // Explanation: The answer is "wke", with the length of 3.
-// Notice that the answer must be a substring, "pwke" is a subsequence and not a substring. 
+// Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
 
+// TIME:O(N) SPACE:O(1)
 const lengthOfLongestSubstring = (str) => {
-  let hash = {};
-  let max = 0;
-  let start = 0;
+	let hash = {};
+	let max = 0;
+	let start = 0;
 
-  for (let i = 0; i < str.length; i++) {
-    //assign current char as key, index as value
-    let currChar = str[i];
+	for (let i = 0; i < str.length; i++) {
+		//assign current char as key, index as value
+		let currChar = str[i];
 
-
-    // need to account for when duplicate is at index 0, falsy value so can't use if curr >=0
-    if (hash[currChar] >= start) {
-    // if duplicate char, start again from last index of the char + 1
-      start = hash[currChar] + 1;
-    }
-    //if not in hash, add and assign index as value
-    hash[currChar] = i;
-    max = Math.max(max, i - start + 1);
-  }
-  return max;
+		// need to account for when duplicate is at index 0, falsy value so can't use if curr >=0
+		if (hash[currChar] >= start) {
+			// if duplicate char, start again from last index of the char + 1
+			start = hash[currChar] + 1;
+		}
+		//if not in hash, add and assign index as value
+		hash[currChar] = i;
+		max = Math.max(max, i - start + 1);
+	}
+	return max;
 };
 
 console.log(lengthOfLongestSubstring('ddwfed')); //4
