ba6a50fc3497a905c7fdbb47f4419f74c899c84f
Selena X <selenaxx20@gmail.com>
2025-05-30 18:51:01 -0700

    init

---
+++ Problem_Solving_Patterns/Two_Pointers/containerWithMostWater.js
@@ -0,0 +1,17 @@
+// Leetcode #11: Container With Most Water
+// You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the line i are at (i, 0) and (i, height[i]).
+
+// Find two lines that together with the x-axis form a container that holds the most water.
+
+// Return the maximum amount of water a container can store.
+
+//input: height = [1,8,6,2,5,4,8,3,7]
+//output: 49
+
+// Explanation:
+
+// The lines at indices 1 and 8 have heights 8 and 7.
+
+// Distance between them is 8 - 1 = 7
+
+// Area = min(8, 7) * 7 = 49 (maximum possible)
