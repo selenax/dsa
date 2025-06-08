ce45c15b5d73351c3f4bbf92cf5a4941250280ec
Selena X <selenaxx20@gmail.com>
2025-06-08 16:54:19 -0700

    solved w/ 2 pointers

--- Problem_Solving_Patterns/Two_Pointers/3Sum.js
+++ Problem_Solving_Patterns/Two_Pointers/3Sum.js
@@ -6,7 +6,7 @@
 // Example 1:
 // Input: nums = [-1,0,1,2,-1,-4]
 // Output: [[-1,-1,2],[-1,0,1]]
-// Explanation: 
+// Explanation:
 // nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
 // nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
 // nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
@@ -22,3 +22,44 @@
 // Input: nums = [0,0,0]
 // Output: [[0,0,0]]
 // Explanation: The only possible triplet sums up to 0.
+
+const threeSum = function (nums) {
+	let result = [];
+	nums.sort((a, b) => a - b);
+
+	if (nums.length < 3) return result;
+
+	// x will be anchored at each iteration as y and z moves around to check for target sum
+	for (let x = 0; x < nums.length - 2; x++) {
+		//skip duplicates
+		if (x > 0 && nums[x] === nums[x - 1]) continue;
+
+		let y = x + 1;
+		let z = nums.length - 1;
+		let target = 0;
+
+		while (y < z) {
+			let sum = nums[x] + nums[y] + nums[z];
+
+			//if sum = target, we have a match, push to result
+			if (sum === target) {
+				result.push([nums[x], nums[y], nums[z]]);
+
+				while (nums[y] === nums[y + 1]) y++; //skip duplicates
+				while (nums[z] === nums[z - 1]) z--;
+				//increment and decrement y and z to check for next sum
+				y++;
+				z--;
+			} else if (sum < target) {
+				y++; //get the next bigger value for y to make sum bigger
+			} else {
+				z--; //get the next smaller value for z to make sum smaller
+			}
+		}
+	}
+	return result;
+};
+
+console.log(threeSum([-1, 0, 1, 2, -1, -4])); //[[-1,-1,2],[-1,0,1]]
+console.log(threeSum([0, 1, 1])); //[]
+console.log(threeSum([0, 0, 0])); //[[0,0,0]]
