d533bc417f4f705fbeb63fd0acec949d139d7701
Selena X <selenaxx20@gmail.com>
2025-06-12 21:23:59 -0700

    completed

--- Problem_Solving_Patterns/Two_Pointers/containerWithMostWater.js
+++ Problem_Solving_Patterns/Two_Pointers/containerWithMostWater.js
@@ -1,17 +1,45 @@
 // Leetcode #11: Container With Most Water
+// Medium
 // You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the line i are at (i, 0) and (i, height[i]).
 
 // Find two lines that together with the x-axis form a container that holds the most water.
 
 // Return the maximum amount of water a container can store.
+// Notice that you may not slant the container.
 
-//input: height = [1,8,6,2,5,4,8,3,7]
-//output: 49
+// Example 1:
+// Input: height = [1,8,6,2,5,4,8,3,7]
+// Output: 49
+// Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
 
-// Explanation:
+// Example 2:
+// Input: height = [1,1]
+// Output: 1
 
-// The lines at indices 1 and 8 have heights 8 and 7.
+// given an array of elements that rep the height (y-axis) at each index (x-axis)
+// find 2 lines on x-axis to form container that will hold most water
+// area = w*l
 
-// Distance between them is 8 - 1 = 7
+const maxArea = (height) => {
+	let max = 0;
+	let left = 0;
+	let right = height.length - 1;
 
-// Area = min(8, 7) * 7 = 49 (maximum possible)
+	while (left < right) {
+		const width = right - left; // base of container
+
+        // choosing the shorter height to make sure it won't slant
+		const area = Math.min(height[left], height[right]) * width; 
+
+		max = Math.max(max, area);
+
+		// move the pointer at the shorter line inward
+		if (height[left] < height[right]) {
+			left++;
+		} else {
+			right--;
+		}
+	}
+
+	return max;
+};
