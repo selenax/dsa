55a0656f9c8f2e402105f211e444c977d7261c14
Selena X <selenaxx20@gmail.com>
2025-06-30 21:59:42 -0700

    Solved w/3 Pointer, O(N^2)

--- Problem_Solving_Patterns/Two_Pointers/3Sum.js
+++ Problem_Solving_Patterns/Two_Pointers/3Sum.js
@@ -23,9 +23,14 @@
 // Output: [[0,0,0]]
 // Explanation: The only possible triplet sums up to 0.
 
+
+
+
 // ALGORITHM: 3 POINTER APPROACH BY SORT + 2 POINTER
+// TIME:O(N^2) SPACE:O(N)
 // input: number[]
 // output: number[][] - each inner array is a unique triplet summing to 0;
+// key - unique indices but values could be repeated, also no repeat triples 
 
 /* use 3 pointers approach - (x,y,z) - anchor x, and move y and z around to find target
 
@@ -42,4 +47,49 @@
 	1. if array has fewer than 3 elements return early w/ []
 */
 
-const threeSum = (nums) => {};
+const threeSum = (nums) => {
+	let result = [];
+
+	// shallow copy of sorted arry
+	const sorted = [...nums].sort((a, b) => a - b);
+
+	// x stops 2 indices before end of array taking into account of y and z 
+	for (let x = 0; x < sorted.length - 2; x++) {
+		// check x duplicates & avoid comparing array[-1]
+		if(x > 0 && sorted[x] === sorted[x-1]) continue;
+
+		let y = x + 1;
+		let z = sorted.length - 1;
+
+		while(y < z) {
+			let sum = sorted[x] + sorted[y] + sorted[z]
+			// found target
+			if(sum === 0) {
+				result.push([sorted[x], sorted[y],sorted[z]]);
+			
+			// check for next duplicate y & z 
+			while(sorted[y] === sorted[y+1]) y++;
+			while(sorted[z] === sorted[z-1]) z--;
+
+			// increment & decrement y and z to check for next sum
+			y++;
+			z--;
+			} else if (sum < 0) {
+				y++
+			} else {
+				z--;
+			}
+		}
+	}
+	return result;
+};
+console.log(threeSum([])) //[]
+console.log(threeSum([-1, 0, 1, 2, -1, -4])); //[[-1,-1,2],[-1,0,1]]
+console.log(threeSum([0, 1, 1])); //[]
+console.log(threeSum([0, 0, 0])); //[[0,0,0]]
+console.log(threeSum([-2, 0, 1, 1, 2])); // [[-2, 0, 2], [-2, 1, 1]]
+console.log(threeSum([-1, 0, 1, 0])); // [[-1, 0, 1]]
+console.log(threeSum([-4, -2, -2, -2, 0, 1, 2, 2, 2, 3, 3, 4, 4, 6, 6]));  
+// [[-4, -2, 6], [-4, 0, 4], [-4, 1, 3], [-4, 2, 2], [-2, -2, 4], [-2, 0, 2]]
+
+
