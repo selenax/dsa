8eb16dc8a10c936c7a70db8fe64cf805dd958919
Selena X <selenaxx20@gmail.com>
2025-07-10 12:20:11 -0700

    solved O(n log n)

--- sorting/meetingRoom.js
+++ sorting/meetingRoom.js
@@ -13,26 +13,58 @@
 // Explanation:
 // Meetings [1,4] and [2,5] overlap with each other.
 
-//O(nlogn + n)
-function canAttendMeetings(intervals) {
-  
-  if(!intervals || intervals.length === 1) return true;
-  //it's overlapping when interval[1] > interval2[0]
-  intervals.sort((a,b) => a[0]-b[0]);
+/*
+input: [][] of integers 
+output: boolean
 
-  // function overlapped(interval1, interval2) {
-  //   return interval1[1] > interval2[0]
-  // }
-  for(let x = 0; x < intervals.length-1; x++) {
-   if(intervals[x][1] > intervals[x+1][0]) {
-     return false;
-  }
-}
-  return true;
-}
+1. sort by start time to process meetings chronologically
+2. if end time is larger than next meeting's start time, there's a overlap - return false
+3. return true
 
+edge case:
+- 1 or less meeting is listed => no overlap, return true
+*/
 
-console.log(canAttendMeetings([[6,7],[2,4],[8,12]]));
-console.log(canAttendMeetings([[1,4],[2,5],[7,9]]));
-console.log(canAttendMeetings([[6,8],[2,6],[1,4]]));
-console.log(canAttendMeetings([[7,10],[2,4]]));
+//TIME:O(n log n); 
+//SPACE:O(1) if sorting in place; but O(n) here since I chose to avoid mutation
+const canAttendMeetings = (intervals) => {
+	if (intervals.length <= 1) return true; //edge case: 0 or 1 meeting
+
+	const sorted = [...intervals].sort((a, b) => a[0] - b[0]);
+
+	for (let i = 1; i < sorted.length; i++) {
+		// compare previous end time with current start time
+		if (sorted[i - 1][1] > sorted[i][0]) {
+			return false; // overlaps
+		}
+	}
+	return true; // no overlap
+};
+
+console.log(
+	canAttendMeetings([
+		[6, 7],
+		[2, 4],
+		[8, 12],
+	])
+); //true
+console.log(
+	canAttendMeetings([
+		[1, 4],
+		[2, 5],
+		[7, 9],
+	])
+); //false
+console.log(
+	canAttendMeetings([
+		[6, 8],
+		[2, 6],
+		[1, 4],
+	])
+); // false
+console.log(
+	canAttendMeetings([
+		[7, 10],
+		[2, 4],
+	])
+); //true
