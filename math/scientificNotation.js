37adf42a01503eca06f5b421f7177d1a4a9ffb58
Selena X <selenaxx20@gmail.com>
2025-07-26 13:07:19 -0700

    solved: manual parsing

--- math/scientificNotation.js
+++ math/scientificNotation.js
@@ -1,8 +1,15 @@
-// write a function interpret 1e-10 in number form
+// write a function interpret 1e-5 in number form
 
-// ask clarifying questions
+/* 
+ask clarifying questions
 
-// 1 times 10 to the power of -10
+1. is this a str or number? (console.log 1e-5 as number returns 0.0005)
+
+2. what does that equal to or can i check to see what it returns in the console
+      -  answer: 0.0001 
+          - follow up: how did you get that answer
+              - answer: 1 times 10 to the power of -5 or 1 * 10^-5
+*/
 
 // method 1: using built in Number() for type conversion
 const scientificNotation = (str) => {
@@ -11,17 +18,23 @@ const scientificNotation = (str) => {
 
 // method 2: manual parsing
 const scientificNotation2 = (str) => {
-  const [baseStr, expStr] = str.toLowerCase().split('e'); // 1E-4 is also valid
+  // split string at e, convert to lower case because 'E' is also value (1E-5)
+  const [baseStr, expStr] = str.toLowerCase().split('e'); //
 
-  const base = parseFloat(baseStr); // 2.5e2
+  // parse string to number
+  const base = parseFloat(baseStr); //
   const exponent = parseInt(expStr, 10);
 
+  // fomula: base * 10^exponent
   let val = base * Math.pow(10, exponent);
 
-  // JS uses 64 bit floating-point math, so instead of returning .0001  JS will return .0000999999999
-  // both of the following methods below rounds and trim trailing zeros
+  // * caveat: JS uses 64 bit floating-point math, so instead of returning .0001  JS will return .0000999999999
+  
+  // both methods below return exact answer
 
-  return Number((+str).toPrecision(10)); // return Number(val.toFixed(10))
+  return Number(val.toFixed(10));  // or 
+  // return Number(val.toPrecision(10));
+ 
 };
 
 console.log(scientificNotation2('1e-5')); // 0.0001
